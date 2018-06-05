f1ae506bff8c4e8ff69f3df58238ea2a6ebd8a0a
Arian Skoki <arian.skoki160@gmail.com>
2018-06-05 13:43:12 +0000

    Repaired bluetooth setup

--- rpi3_bluetooth_setup.sh
+++ rpi3_bluetooth_setup.sh
@@ -1,20 +1,21 @@
+
 #!/bin/bash
 mac_address='A0:E9:DB:08:C1:BF'
 mac_address_=$(echo "$mac_address" | tr ':' '_')
 
 coproc bluetoothctl
-echo -e 'power on $mac_address' >&${COPROC[1]}
+echo -e 'power on' >&${COPROC[1]}
 
-echo -e 'agent on $mac_address' >&${COPROC[1]}
+echo -e 'agent on' >&${COPROC[1]}
 
-echo -e 'default-agent $mac_address' >&${COPROC[1]}
+echo -e 'default-agent' >&${COPROC[1]}
 
 sudo killall bluealsa
 pulseaudio --start
 
-echo -e 'pair $mac_address' >&${COPROC[1]}
-echo -e 'trust $mac_address' >&${COPROC[1]}
-echo -e 'connect $mac_address' >&${COPROC[1]}
+echo -e 'pair '$mac_address >&${COPROC[1]}
+echo -e 'trust '$mac_address >&${COPROC[1]}
+echo -e 'connect '$mac_address >&${COPROC[1]}
 
 pacmd set-card-profile bluez_card.$mac_address_ a2dp_sink
 pacmd set-default-sink bluez_sink.$mac_address_.a2dp_sink
@@ -25,3 +26,4 @@ pacmd set-card-profile bluez_card.$mac_address_ headset_head_unit
 
 pacmd set-default-sink bluez_sink.$mac_address_.headset_head_unit
 pacmd set-default-source bluez_source.$mac_address_.headset_head_unit
+
